1112085cdd3e28bebc4c4c8e89e25a7bd8606385
Caio Reis <caionohamachi@gmail.com>
2023-11-15 15:08:23 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.dto.GameListDto;
+import com.devsuperior.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+	
+	@Autowired
+	private GameListService gameListService;
+		
+	@GetMapping
+	public List<GameListDto> findAll(){
+		List<GameListDto> result = gameListService.findAll();
+		return result;
+	}
+}
